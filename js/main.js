7e19220d3ec42fae7a4996a2d7e3c20f5e9a6068
CoderDi <lvbnhbq2103@mail.ru>
2019-05-04 14:33:03 +0700

    kreditnye-karty ready

--- js/main.js
+++ js/main.js
@@ -1,8 +1,62 @@
 $(document).ready(function () {
 
+ 
+  
+  var updateColl
+  $.fn.liColl = function(options){
+    // настройки по умолчанию
+    var o = $.extend({
+      n_coll: 4, //количество колонок
+      c_unit: '%', //Единица измерения
+      c_width: 300, //Ширина колонок
+      p_left: 5 //отступ слева %
+    },options);
+    updateColl = function update(listWrap){
+      listWrap.children('.coll_s').each(function(){
+        $(this).children().unwrap();	
+      })
+      listWrap.liColl(options);
+    }
+    return this.each(function(){
+      element = $(this).css({overflow:'hidden'});
+      nc = o.n_coll;
+      pl = o.p_left;
+      i = 1;
+      c_un = 'px';
+      if(options.c_unit != c_un){
+        coll_w = Math.floor(100/nc);
+        coll_w = coll_w - pl;
+      }else{
+        coll_w = options.c_width - pl;
+      };
+      num_1 = element.children('li').length;
+      create();
+      function create(){
+        n_end = Math.ceil(num_1/nc);
+        var cc = $('<div />').addClass("coll_s c_" + i);
+        //element_2.append();
+        element.children('li').slice(0,n_end).wrapAll(cc);
+        if(num_1 != n_end){
+          i++;
+          nc--;
+          num_1 = num_1 - n_end;
+          create();
+        };
+      };
+    });
+  };
+
+  $('.cities__list').liColl({
+    c_unit: '%', // '%' или 'px' При указании '%' — ширина 'c_width' игнорируется
+    n_coll: 4, //количество колонок
+    p_left: 0 //отступ слева %						
+  });
+  updateColl($('.cities__list')) //обновить список
+  
+
   $(".js-slider").slick({
     dots: true,
-    autoPlay: true,
+    autoplay: true,
     autoplaySpeed: 5000,
     adaptiveHeight: true,
     responsive: [
@@ -21,7 +75,12 @@ $(document).ready(function () {
   });
   $(".breadcrumbs__item--current").removeAttr('href');
 
-
+  $(".pickup__btn").click(function(){
+    $(this).parents(".pickup").find(".pickup__popup").addClass("pickup__popup--open");
+  });
+  $(".pickup__close").click(function(){
+    $(this).parent(".pickup__popup").removeClass("pickup__popup--open");
+  });
 
 
 
