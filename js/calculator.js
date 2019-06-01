b50685c750160cea7b4bf72da7b15af440f6bfd9
CoderDi <lvbnhbq2103@mail.ru>
2019-06-02 02:03:40 +0700

    add scripts credit, zaym, refin

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,20 @@ function numSpacing(num) {
   return String(parseInt(num)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')
 }
 
+function format(digits){
+  var value=this;
+  if(typeof digits!='undefined'){
+    return parseFloat(value).toFixed(digits).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g,'$1 ')
+  }
+  return parseFloat(value).toLocaleString()
+}
+Number.prototype.format=format;
+String.prototype.format=format;
+
+function normaleNum(num) {
+  return parseFloat(num.replace(' ', ''));
+}
+
 function initSelectDate() {
   if ($("#day").length > 0) {
     var day = new Date,
@@ -60,31 +74,66 @@ $(document).ready(function () {
     $(this).removeClass("calculator__select--active");
   });
 
-  $('.input-slider').each(function (i, b) {
-    var max = $(b).data('max'), 
-        min = $(b).data('min'), 
-        suffix = $(b).data('suffix'),
-        value = $(b).data('value'),
-        step = $(b).data('step'),
-        parent = $(b).parents('.calculator__block'),
-        input = $('.calculator__input', parent);
-    if (!(suffix)) {
-      suffix = '';
-    }
-    $(b).slider({
-      animate: "slow",
-      range: "min",
-      min: min,
-      max: max,
-      step: step,
-      value: value,
-      slide : function(event, ui) {    
-        input.val(numSpacing(ui.value) + ' ' + suffix);       
+
+  function uiSlider(selector) {
+    $(selector).each(function (i, b) {
+      var max = $(b).data('max'), 
+          min = $(b).data('min'), 
+          suffix = $(b).data('suffix'),
+          value = $(b).data('value'),
+          step = $(b).data('step'),
+          parent = $(b).parents('.calculator__block'),
+          input = $('.calculator__input', parent);
+      if (!(suffix)) {
+        suffix = '';
       }
+      $(b).slider({
+        animate: "slow",
+        range: "min",
+        min: min,
+        max: max,
+        step: step,
+        value: value,
+        slide : function(event, ui) {    
+          input.val(numSpacing(ui.value) + ' ' + suffix);       
+        }
+      });
+    
+      input.val(numSpacing(value) + ' ' + suffix);
     });
-  
-    input.val(numSpacing(value) + ' ' + suffix);
+  }
+
+
+  var purchaseCount = 1,
+      newId;
+  $(".purchase__list").on("click",".purchase-add", function() {
+    purchaseCount++;
+    newId = "purchase" + purchaseCount;
+    var selector = '#' + newId + ' .input-slider';
+    $(purchaseBlock).clone().appendTo(".purchase__list")
+      .attr("id", newId)
+      .find(".purchase__caption span").text(purchaseCount);
+    
+    uiSlider(selector);
+    console.log($(".purchase").length);
+
+    var scTop = $('#' + newId).offset().top - 100;
+    console.log(scTop);
+    $("html, body").animate({scrollTop: scTop})
   });
+  $(".purchase__list").on("click",".purchase-remove", function(){
+    purchaseCount--;
+    newId = "purchase" + purchaseCount;
+    var scTop = $('#' + newId).offset().top - 100;
+    console.log(scTop);
+    $("html, body").animate({scrollTop: scTop})
+    $(this).parents(".purchase").remove();
+  });
+
+  var purchaseBlock = $("#purchase1").clone(true);
+  uiSlider('.input-slider');
+  
+  
 
 
 
@@ -114,28 +163,47 @@ $(document).ready(function () {
         break;
 
       case "credit":
-        var creditPercents = $(then).find(".js-percents").val(),
-            creditPeriod = $(then).find(".js-period").val(),
-            creditMethod = $(then).find("input[name='method']:checked").val(),
+        var creditPercents = normaleNum($(then).find(".js-percents").val()),
+            creditPeriod = normaleNum($(then).find(".js-period").val()),
+            creditMethod = normaleNum($(then).find("input[name='method']:checked").val()),
             creditDateStart = $(then).find("select[name='month']").val() + '.' + $(then).find("select[name='year']").val(),
             creditVidPay = $(then).find("input[name='vid-pay']:checked").val();
-        console.log("creditMethod = " + creditMethod);
         if (creditMethod == 0) {
-          var creditSumm = $(then).find(".js-credit-summ").val();
+          var creditSumm = normaleNum($(then).find(".js-credit-summ").val());
           calcCreditSumm(creditSumm,creditPercents,creditPeriod,creditDateStart,creditVidPay)
         } else if (creditMethod == 1) {
-          var creditPurchaseSumm = $(then).find(".js-purchase-summ").val(),
-              creditInitialPay = $(then).find(".js-initial-pay").val();
-          calcCreditPurchase(creditPurchaseSumm,creditInitialPay,creditPercents,creditPeriod,creditDateStart,creditVidPay)
+          var creditPurchaseSumm = normaleNum($(then).find(".js-purchase-summ").val()),
+              creditInitialPay = normaleNum($(then).find(".js-initial-pay").val());
+          $('.calccredit_diff').text(numSpacing(creditPurchaseSumm - creditInitialPay)); 
+          calcCreditSumm(creditPurchaseSumm - creditInitialPay,creditPercents,creditPeriod,creditDateStart,creditVidPay);
         }
+        $('.js-periodPay').text(numSpacing(creditPeriod) + " мес."); 
+          
         break;
 
       case "zaym":
-        var zaymSumm = $(then).find(".js-zaym-summ").val(),
-            zaymPeriod = $(then).find(".js-zaym-period").val(),
-            zaymPercent = $(then).find(".js-zaym-percent").val(),
-            zaymDateStart = $(then).find("select[name='day']").val() + '.' + $(then).find("select[name='month']").val();
-        calcZaym(zaymSumm,zaymPeriod,zaymPercent,zaymDateStart)
+        var zaymSumm = normaleNum($(then).find(".js-zaym-summ").val()),
+            zaymPeriod = normaleNum($(then).find(".js-zaym-period").val()),
+            zaymPercent = normaleNum($(then).find(".js-zaym-percent").val()),
+            zaymDateStart = $(then).find("select[name='month']").val() + '.' + $(then).find("select[name='day']").val();
+        calcZaym(zaymSumm,zaymPeriod,zaymPercent,zaymDateStart);
+        break;
+      
+      case "refin":
+        var refinSumm = normaleNum($(then).find(".js-credit-summ").val()), 
+            refinPeriod = normaleNum($(then).find(".js-period").val()), 
+            refinPercent = normaleNum($(then).find(".js-percents").val()), 
+            refinPercentNew = normaleNum($(then).find(".js-percents-new").val()), 
+            refinType = normaleNum($(then).find("input[name='method']:checked").val());
+        if (refinType == 0) {
+          $(".calculator__res-line0").hide();
+          $(".calculator__res-line1").show();
+        } else {
+          $(".calculator__res-line1").hide();
+          $(".calculator__res-line0").show();
+        }
+
+        calcRefin(refinSumm,refinPeriod,refinPercent,refinPercentNew,refinType)
         break;
       
       default:
@@ -144,6 +212,17 @@ $(document).ready(function () {
   });
 
   /*Calculators*/
+  function formatMoney(money, bool) {
+    var format  = String(money);
+    format = format.replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g,"\$1 ");
+    if (bool) {
+      return format
+    } else {
+      return format + ' ₽';;
+    }
+  }
+
+
   function calcCardMinAmount(cardMinAmount,cardCreditSumm,cardMaxLimit,cardPercents) {
     
   }
@@ -156,16 +235,198 @@ $(document).ready(function () {
     
   }
 
+  // Калькулятор кредита
   function calcCreditSumm(creditSumm,creditPercents,creditPeriod,creditDateStart,creditVidPay) {
-    
-  }
+    function calcPay(sum, percent, month,  date, type) {
+      var calcValue = {};
+      calcValue.data = {
+        percent:Number(percent/1200),
+        inputSum: Number(sum),
+        inputMonth: Number(month),
+        typeCalc: Number(type),
+        date: date
+      }
+      
+      if(calcValue.data.typeCalc == 0) {
+        calcValue.monthPay = calcValue.data.inputSum * (calcValue.data.percent + calcValue.data.percent /
+          (Math.pow((1 + calcValue.data.percent), calcValue.data.inputMonth) - 1)); // платеж в месяц
+        calcValue.totalPay = calcValue.monthPay * calcValue.data.inputMonth;
+      } else {
+        calcValue.mainPay = calcValue.data.inputSum/calcValue.data.inputMonth;
+        calcValue.totalPay = 0;
+        for(var i = 1;i<calcValue.data.inputMonth+1;i++){
+          calcValue.totalPay = calcValue.totalPay +
+            decreasingMonthPay(i, calcValue.data.inputSum, calcValue.data.inputMonth,calcValue.data.percent);
+        }
+        calcValue.totalPay = calcValue.totalPay + calcValue.data.inputSum;
+      }
 
-  function calcCreditPurchase(creditPurchaseSumm,creditInitialPay,creditPercents,creditPeriod,creditDateStart, creditVidPay) {
-    
-  }
+      calcValue.morePay = calcValue.totalPay-(calcValue.data.inputSum);
+      calcValue.overPay = calcValue.morePay/calcValue.data.inputSum*100;
 
+      return calcValue
+    }
+    //отрисовка значений
+    function showValue(data) {
+      if(data.data.typeCalc == 0) {
+          $('.js-monthPay').text(formatMoney( Math.round(data.monthPay)) );
+      }
+      else
+          $('.js-monthPay').text('от '+formatMoney( Math.round(data.mainPay) ));
+
+      $('.js-totalPay').text(formatMoney( Math.round(data.totalPay)) );
+      $('.js-morePay').text(formatMoney( Math.round(data.morePay)) );
+    }
+    //герерация таблицы
+    function generateTable(info) {
+      var remTotal = info.data.inputSum,
+          remDebt = info.data.inputSum,
+          payPercent,paySum,monthPay,pp = 0,
+          currDate = new Date("01." + info.data.date);
+
+      $('.grafik__table table').remove();
+      $(".grafik").show();
+
+
+      var table = '<table>' +
+          '<tr>' +
+          '<th>№, Месяц</th>' +
+          '<th><span class="th-desktop">Сумма платежа</span><span class="th-mobile">Платеж</span></th>' +
+          '<th>Проценты + долг</th>' +
+          '<th>Остаток долга</th>' +
+          '</tr>';
+
+      for(var i = 1; i<info.data.inputMonth+1;i++){
+        if(info.data.typeCalc == 0) {
+            monthPay = info.monthPay;
+            payPercent = remDebt * info.data.percent;
+            paySum = monthPay-payPercent;
+            remDebt = remDebt-paySum;
+        }
+        else {
+            payPercent = decreasingMonthPay(i, remDebt, info.data.inputMonth,info.data.percent);
+            monthPay = info.mainPay + payPercent;
+            paySum = info.mainPay;
+        }
+        remTotal= remTotal-paySum;
+        currDate.setMonth(currDate.getMonth() + 1);
+
+        var row = '';
+        row += '<tr>' +
+          '<td>' + i + '. ' + formatDate(currDate)+'</td>'+
+          '<td>' +
+          '<span class="td-desktop">'+formatMoney( Math.round(monthPay),true )+'</span>' +
+          '<span class="td-mobile">'+formatMoney( Math.round(payPercent),true )+ ' + ' +
+          formatMoney( Math.round(paySum),true ) + '</span>' +
+          '</td>' +
+          '<td>' +formatMoney( Math.round(payPercent),true )+ ' + ' +
+          formatMoney( Math.round(paySum),true ) + '</td>'+
+          '<td>'+formatMoney( Math.round(remTotal),true )+'</td>'+
+        '</tr>';
+        table += row;
+      }
+      table +='</table>';
+      $('.grafik__table').html(table);
+    }
+    function decreasingMonthPay(number,sum,month,percent) {
+      return (sum - (number-1)* (sum/month))*percent;
+    }
+    function formatDate(date) {
+      var dd = date.getDate();
+      if (dd < 10) dd = '0' + dd;
+      var mm = date.getMonth() + 1;
+      if (mm < 10) mm = '0' + mm;
+      var yy = date.getFullYear();
+      return dd + '.' + mm + '.' + yy;
+    }
+    
+    showValue(calcPay(creditSumm,creditPercents,creditPeriod,creditDateStart,creditVidPay));
+    generateTable(calcPay(creditSumm,creditPercents,creditPeriod,creditDateStart,creditVidPay));
+  }  
+
+  // Калькулятор займа
   function calcZaym(zaymSumm,zaymPeriod,zaymPercent,zaymDateStart) {
-    
+    var now = new Date(zaymDateStart),
+        result1 = zaymSumm,
+        result2 = zaymSumm+(zaymSumm/100*zaymPercent*zaymPeriod),
+        date = new Date(+now+(zaymPeriod*86400*1000)),
+        d = date.getDate(),
+        m = date.getMonth()+1,
+        y = new Date().getFullYear();
+
+    if (d < 10) d = '0'+d;
+    if (m < 10) m = '0'+m;
+
+    var result3 = d+'.'+m+'.'+y,
+        result4 = zaymPeriod;
+
+    switch (result4 % 10) {
+      case 1:
+        if (result4 == 11) {
+          result4 += " дней";
+        } else {
+          result4 += " день";
+        }
+        break;
+      case 2:
+      case 3:
+      case 4:
+        if ((result4 > 10)&((result4 < 15))) {
+          result4 += " дней";
+        } else {
+          result4 += " дня";
+        }
+        break;
+      default:
+        result4 += " дней";
+        break;
+    }
+    // Вывод
+    $('.calczaim_result1').text(result1.format()+' ₽'); // Вы берете
+    $('.calczaim_result2').text(result2.format()+' ₽'); // Отдаете
+    $('.calczaim_result3').text(result3);                  // Вернуть до
+    $('.calczaim_result4').text(result4); 
+  }
+
+  // Калькулятор рефинансирования
+  function calcRefin(refinSumm,refinPeriod,refinPercent,refinPercentNew,refinType) {
+    var debt = refinSumm,
+        years = refinPeriod,
+        rate = refinPercent,
+        rateRefin = refinPercentNew,
+        period = years,
+        k = rate / 100 / 12,
+        kAnnyit = k * Math.pow((1 + k), period) / (Math.pow((1 + k), period) - 1),
+        monthlyPayment = kAnnyit * debt,
+        overpayment = monthlyPayment * period - debt,
+        periodRefin = null,
+        kRefin = rateRefin / 100 / 12,
+        kAnnyitRefin = null,
+        monthlyPaymentRefin = null,
+        overpaymentRefin = null,
+        yearsDecrease = 0;
+  
+    if (refinType == 1) {
+      kAnnyitRefin = kRefin * Math.pow((1 + kRefin), period) / (Math.pow((1 + kRefin), period) - 1);
+      monthlyPaymentRefin = kAnnyitRefin * debt;
+      overpaymentRefin = monthlyPaymentRefin * period - debt;
+    }
+
+    if (refinType == 0) {
+      monthlyPaymentRefin = monthlyPayment;
+      periodRefin = Math.log(monthlyPaymentRefin / (monthlyPaymentRefin - debt * kRefin)) / Math.log(kRefin + 1);
+      overpaymentRefin = monthlyPaymentRefin * periodRefin - debt;
+      yearsDecrease = (period - periodRefin) ;
+    }
+
+    var overpaymentDecrease = overpayment - overpaymentRefin,
+        monthlyPaymentDecrease = monthlyPayment - monthlyPaymentRefin;
+
+    $('.js-monthly-payment').text(formatMoney( Math.round(monthlyPaymentRefin)) );
+    $('.js-overpayment').text(formatMoney( Math.round(overpaymentRefin)) );
+    $('.js-overpayment-decrease').text(formatMoney( Math.round(overpaymentDecrease)) );
+    $('.js-years-decrease').text(Math.round(yearsDecrease) + ' мес.' );
+    $('.js-montlypay-decrease').text(formatMoney(Math.round(monthlyPaymentDecrease)));
   }
 
 
