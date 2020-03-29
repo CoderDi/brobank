e0c50a043671303012f33f9fa5bcc44c46bddc02
CoderDi <lvbnhbq2103@mail.ru>
2020-03-30 00:31:19 +0700

    commit

---
+++ js/new-script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+  $(".f-region__title").click(function(){
+    $(".f-cities").not($(this).parent(".f-region").find(".f-cities")).slideUp(200);
+    $(".f-region__title").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+    $(this).parent(".f-region").find(".f-cities").slideToggle(200);
+  });
+
+  $(".f-drop__title").click(function(){
+    $(this).parent(".f-drop").toggleClass("active");
+  });
+
+  $(".f-drop__item").click(function(){
+    $(this).parents(".f-drop").find(".f-drop__title strong").text($(this).text());
+    $(this).parents(".f-drop").find(".f-drop__item").removeClass("active");
+    $(this).addClass("active");
+    $(this).parents(".f-drop").removeClass("active");
+  });
+
+  $(".filter-drop__item").click(function(){
+    $(this).parents(".filter-item").find(".filter-item__value").val($(this).text());
+    $(this).parents(".filter-item").find(".filter-drop__item").removeClass("active");
+    $(this).addClass("active");
+    $(this).parents(".filter-item").removeClass("active");
+  });
+
+  $(".filter-item__top").click(function(){
+    $(".filter-item").not($(this).parent(".filter-item")).removeClass("active");
+    $(this).parents(".filter-item").toggleClass("active");
+  });
+
+  $(".f-ready__link--top").click(function(){
+    $(this).parents(".f-ready__item").toggleClass("active");
+    $(this).parents(".f-ready__item").find(".f-ready__item_content").slideToggle(200);
+  });
+});
