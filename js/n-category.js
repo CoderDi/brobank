8c8447318f7b01f3bd4d2d25474c681c8af5310a
CoderDi <lvbnhbq2103@mail.ru>
2021-09-05 02:49:36 +0700

    commit

--- js/n-category.js
+++ js/n-category.js
@@ -8,12 +8,25 @@ $(document).ready(function () {
   $(".js-category-card-calc").on("click", function(){
     $("#popupCalc").addClass("popup--show");
     
-$("#limit-content").css("height", $("#limitem1").outerHeight());
+  $("#limit-content").css("height", $("#limitem1").outerHeight());
   });
 
   $(".top__text-more").on("click", function(){
     $(this).hide().parents(".top__text").addClass("active")
   });
+
+
+  $(document).scroll(function(){
+    var w;
+    if ($(this).scrollTop() >= $("#offerLink").offset().top - $(window).height()) {
+      w = ($(this).scrollTop() - $("#offerLink").offset().top + $(window).height()) / $(window).height() * 100;
+      $("#offerLink").css("width", w + "%");
+    }
+
+    if ($(this).scrollTop() >= $("#offerLink").offset().top) {
+      document.location.href = $("#offerLink").attr('href');
+    }
+  })
 
 
 });
